27c254f3ecca24e6ac8f8e91f5dcef80c02a3068
imanamiri-v <imanamiri-iran-lover@gmail.com>
2026-06-17 00:10:23 +0330

    give IDs in the js file

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+const form = document.getElementById("form");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const mail = document.getElementById("mail");
+const job = document.getElementById("job");
+const phoneNumber = document.getElementById("phoneNumber");
+const gender = document.getElementById("gender");
 
 
