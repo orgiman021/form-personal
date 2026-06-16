0ffc3874fc1cf7bcb5cf837931a9f63eafbeb188
imanamiri-v <imanamiri-iran-lover@gmail.com>
2026-06-17 00:49:58 +0330

    bulid show info

--- script.js
+++ script.js
@@ -28,5 +28,29 @@ form.addEventListener("submit", (event) => {
   users.push(personsInfo);
   form.reset();
 });
-// show user info
+// show user info in modal box
+showModal.addEventListener("click", () => {
+  if (users.length === 0) {
+    modal.innerHTML = `<h3>Registration not yet completed</h3>`;
+  } else {
+    modal.innerHTML = `<h3>users list : </h3>`;
+    const list = document.createElement("ul");
 
+    users.forEach((persons, index) => {
+        const li = document.createElement("li");
+        const { name, email, job, phoneNumber, gender } = persons;
+        li.innerText = `${index + 1}
+        name : ${persons.name}
+        email : ${persons.email}
+        job : ${persons.job}
+        phone number : ${persons.phoneNumber}
+        gender : ${persons.gender}`;
+        list.appendChild(li);
+    });
+    modal.appendChild(list);
+}
+    overlay.style.opacity = "1";
+    overlay.style.visibility = "visible";
+    modal.style.opacity = "1";
+    modal.style.visibility = "visible";
+});
