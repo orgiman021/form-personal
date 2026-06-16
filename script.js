d7b63b1efebbeadfcdc99598576e7f5b116c47cc
imanamiri-v <imanamiri-iran-lover@gmail.com>
2026-06-17 00:40:49 +0330

    correction condition

--- script.js
+++ script.js
@@ -5,23 +5,28 @@ const mail = document.getElementById("mail");
 const job = document.getElementById("job");
 const phoneNumber = document.getElementById("phoneNumber");
 const gender = document.getElementById("gender");
-
+const showModal = document.getElementById("showModal");
+const overlay = document.getElementById("overlay");
+const modal = document.getElementById("modal");
 
 let users = [];
-form.addEventListener('submit',(event)=>{
-    event.preventDefault();
-const personsInfo = {
-    name : `${firstName.value.trim()} ${lastName.value.trim()}`,
-    email : mail.value.trim(),
-    job : job.value.trim(),
-    phoneNumber : phoneNumber.value.trim(),
-    gender : gender.value.trim(),
-}
-if(personsInfo.name || personsInfo.email){
-    alert("Entering name and email is required ")
-}
+// add user
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const personsInfo = {
+    name: `${firstName.value.trim()} ${lastName.value.trim()}`,
+    email: mail.value.trim(),
+    job: job.value.trim(),
+    phoneNumber: phoneNumber.value.trim(),
+    gender: gender.value.trim(),
+  };
+  if (!personsInfo.name || !personsInfo.email) {
+    alert("Entering name and email is required ");
+    return;
+  }
 
-users.push(personsInfo)
-form.reset();
-
+  users.push(personsInfo);
+  form.reset();
 });
+// show user info
+
